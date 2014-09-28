ce47400219b839f40b9e7d0eb247bc708e6dbad2
Eric Dobson <eric.n.dobson@gmail.com>
2014-09-28 16:49:02 -0700

    Make enum parsing code use clang instead of old c parser.

--- private/ffi/enums.rkt
+++ private/ffi/enums.rkt
@@ -4,42 +4,18 @@
   (for-syntax
     racket/base
     racket/list
+    racket/string
     racket/syntax
     racket/match
     racket/string
     syntax/parse
     "../llvm-headers.rkt")
-  (rename-in racket/contract
-    (-> c:->))
-  ffi/unsafe)
-
-(define-for-syntax declarations (read-header "llvm-c/Core.h"))
-
-(define-for-syntax (enum-list->symbols enum-list)
-  (match enum-list
-    ((list-rest (? symbol? name) '= (? number? val)  rest)
-     (cons name (enum-list->symbols rest)))
-    ((cons (? symbol? name) rest)
-     (cons name (enum-list->symbols rest)))
-    ((list) null)))
-
-
-(define-for-syntax (generate-enum-code nm enum-list)
-  (define/with-syntax name nm)
-  (define/with-syntax name? (format-id nm "~a?" nm))
-
-  (define quoted-enum-symbols
-    (for/list ((symbol (enum-list->symbols enum-list)))
-      #`'#,symbol))
-  #`(begin
-     (provide name name?)
-     (define name (_enum '#,enum-list))
-     (define name? (or/c #,@quoted-enum-symbols))))
+  racket/contract
+  (only-in ffi/unsafe _enum))
 
 (define-for-syntax (generate-enum-code2 nm enum-values)
   (define/with-syntax name nm)
   (define/with-syntax name? (format-id nm "~a?" nm))
-
 
   (define enum-list
     (append*
@@ -53,21 +29,53 @@
      (define name (_enum '#,enum-list))
      (define name? (or/c #,@quoted-enum-symbols))))
 
-
-
-(define-syntax define-enum
-  (syntax-parser
-    ((_ name:id)
-     (generate-enum-code2
-       #'name
-       (extract-enum-values (syntax->datum #'name) declarations)))))
+(begin-for-syntax
+  (define (namespace-get-namespaces ns names)
+    (for/fold ([ns ns]) ([name names])
+       (namespace-get-namespace ns name)))
+  (define-syntax-class namespaced-id
+    #:attributes (name namespaces)
+    (pattern name:id
+      #:attr namespaces '())
+    (pattern (nss:id ... name:id)
+      #:attr namespaces (syntax->datum #'(nss ...))))
+  (define-splicing-syntax-class declaration
+    #:attributes (generator)
+    (pattern (~seq :namespaced-id
+                   (~optional (~seq #:name alt-name))
+                   (~optional (~seq #:rename rename) #:defaults ([rename #'values])))
+      #:with external-name (or (attribute alt-name) #'name)
+      #:with external-predicate (format-id #'external-name "~a?" #'external-name)
+      #:attr generator
+        (λ (ns)
+          (generate-enum-code2
+            #'external-name
+            (namespace-get-typedef (namespace-get-namespaces ns (attribute namespaces))
+                                   (syntax-e #'name)))))
+    (pattern (~seq #:enum :namespaced-id
+                   (~optional (~seq #:name alt-name))
+                   (~optional (~seq #:rename rename) #:defaults ([rename #'values]))
+                   )
+      #:with external-name (or (attribute alt-name) #'name)
+      #:with external-predicate (format-id #'external-name "~a?" #'external-name)
+      #:attr generator
+        (λ (ns)
+          (generate-enum-code2
+            #'external-name
+            (namespace-get-enum (namespace-get-namespaces ns (attribute namespaces))
+                                (syntax-e #'name)))))))
 
 (define-syntax define-enums
   (syntax-parser
-    ((_ names:id ...)
-     #'(begin (define-enum names) ...))))
+    [(_ header-file:str #:lang lang:id decl:declaration ...)
+     (define ns (read-header (syntax-e #'header-file) #:lang (syntax-e #'lang)))
+     #`(begin
+         #,@(for/list ([gen (in-list (attribute decl.generator))])
+              (gen ns)))]))
+(define-for-syntax  (underscore->hypen symbol)
+  (string->symbol (string-replace (symbol->string symbol) "_" "-")))
 
-(define-enums
+(define-enums "llvm-c/Core.h" #:lang c
   LLVMAttribute
   LLVMOpcode
   LLVMTypeKind
@@ -78,18 +86,6 @@
   LLVMRealPredicate
   LLVMLandingPadClauseTy)
 
-(define-syntax generate-intrinsics
-  (syntax-parser
-    ((_ name:id)
-     (define intrinsic-names
-       (cons 'not-intrinsic
-         (map (compose string->symbol
-                       (lambda (x) (regexp-replace "_" x "-"))
-                       string-trim)
-           (string-split
-             (read-genfile "llvm/IR/Intrinsics.gen" "GET_INTRINSIC_ENUM_VALUES")
-             ","))))
-     (generate-enum-code #'name intrinsic-names))))
-(generate-intrinsics LLVMIntrinsic)
-
+(define-enums "llvm/IR/Intrinsics.h" #:lang c++
+  #:enum (llvm Intrinsic ID) #:name LLVMIntrinsic #:rename underscore->hyphen)
 
--- private/llvm-headers.rkt
+++ private/llvm-headers.rkt
@@ -2,7 +2,6 @@
 
 (require
   (for-syntax racket/base)
-  (except-in (planet dherman/c:4:0) struct)
   (rename-in racket/contract
     (-> c:->))
   racket/port
@@ -15,76 +14,72 @@
 
 (provide 
   (contract-out
-    (read-genfile (c:-> path-string? string? string?))
-    (read-header (c:-> path-string? (listof enum-decl?)))
-    (extract-enum-values (c:-> symbol? (listof enum-decl?) (hash/c symbol? integer?)))))
+    (read-header (c:-> path-string? #:lang (or/c 'c 'c++) any/c))
+    (namespace-get-enum (c:-> namespace? symbol? (or/c (hash/c symbol? integer?) #f)))
+    (namespace-get-typedef (c:-> namespace? symbol? (or/c (hash/c symbol? integer?) #f)))
+    (namespace-get-namespace (c:-> namespace? symbol? (or/c namespace? #f)))))
 
-(define (read-genfile name def)
-  (define (munge contents)
-    (string-join
-      (filter
-        (lambda (str) (> (string-length str) 0))
-        (map
-          (lambda (line)
-            (string-trim (regexp-replace "//.*" line "")))
-          (string-split contents "\n")))
-      "\n"))
+;; Represents the enums, typedefs, and subname spaces of a namespace.
+;; enums: (hash/c symbol? integer?)
+;; typedefs: (hash/c symbol? integer?)
+;; namespaces (hash/c symbol? namespace?)
+(struct namespace [enums typedefs namespaces] #:transparent)
 
-
-  (define filename (string-append (llvm-include-dir) "/" name))
-
-
-  (define command `("/usr/bin/env" "cpp" ,(format "-D~a" def) "-E" "-P" ,@(llvm-cpp-flags) ,filename))
-  (let-values (((process out in err) (apply subprocess #f #f #f command)))
-   (close-output-port in)
-   (begin0
-    (munge (string-trim (port->string out)))
-    (let ((err-string (string-trim (port->string err))))
-      (close-input-port err)
-      (close-input-port out)
-      (subprocess-wait process)
-      (unless (= (subprocess-status process) 0)
-        (error 'cpp "Returned non zero exit code for file: ~a\n~a" filename err-string))))))
-
-;; Kind is either 'enum or 'typedef
-;; Name is a symbol and the name of the enum
-;; Values is a dict from symbols to integers
-(struct enum-decl [kind name values])
-
-(define (read-header name)
+(define (read-header name #:lang lang)
   (define filename (string-append (llvm-include-dir) "/" name))
   (define idx (create-index #:display-diagnostics #t))
-  (define tu (create-translation-unit-from-source-file idx filename (llvm-cpp-flags)))
+  (define tu (create-translation-unit-from-source-file idx filename
+                                                       (list* "-x" (symbol->string lang) (llvm-cpp-flags))))
 
-  (define (enum-decl->_enum cursor)
+  ;; cursor -> (hash/c symbol? integer?)
+  (define (read-enum-decl cursor)
     (make-immutable-hash
       (cursor-map cursor
         (λ (child)
           (cons (string->symbol (cursor-spelling child)) (enum-constant-decl-value child))))))
 
-  (define result
-    (filter values
-      (cursor-map (translation-unit-cursor tu)
+  ;; cursor -> namespace
+  (define (visit-cursor c)
+    (define entries
+      (cursor-map c
         (λ (cursor)
            (cond
+             [(namespace-decl-cursor? cursor)
+              (list 'namespace
+                    (string->symbol (cursor-spelling cursor))
+                    (visit-cursor cursor))]
              [(typedef-decl-cursor? cursor)
-              (define enum (cursor-find cursor enum-decl->_enum))
-              (and enum (enum-decl 'typedef (string->symbol (cursor-spelling cursor)) enum))]
+              (define enum (cursor-find cursor read-enum-decl))
+              (and enum (list 'typedef (string->symbol (cursor-spelling cursor)) enum))]
              [(enum-decl-cursor? cursor)
               (define name (cursor-spelling cursor))
               (and (not (equal? "" name))
-                   (enum-decl 'enum (string->symbol name) (enum-decl->_enum cursor)))]
-             [else #f])))))
-  result
-  )
+                   (list 'enum (string->symbol name) (read-enum-decl cursor)))]
+             [else #f]))))
+    (namespace
+      (for/hash ([entry (in-list entries)]
+                 #:when (and entry (equal? (first entry) 'enum)))
+        (values (second entry) (third entry)))
+      (for/hash ([entry (in-list entries)]
+                 #:when (and entry (equal? (first entry) 'typedef)))
+        (values (second entry) (third entry)))
+      (for/hash ([entry (in-list entries)]
+                 #:when (and entry (equal? (first entry) 'namespace)))
+        (values (second entry) (third entry)))))
 
+ (visit-cursor (translation-unit-cursor tu)))
 
-(define (find-typedef name declarations)
-  (for/first ([decl declarations]
-           #:when (equal? (enum-decl-kind decl) 'typedef)
-           #:when (equal? (enum-decl-name decl) name))
-    decl))
+(define (namespace-get-typedef namespace name)
+  (for/first ([(n v) (in-hash (namespace-typedefs namespace))]
+              #:when (equal? name n))
+    v))
 
+(define (namespace-get-enum namespace name)
+  (for/first ([(n v) (in-hash (namespace-enums namespace))]
+              #:when (equal? name n))
+    v))
 
-(define (extract-enum-values name declarations)
-  (enum-decl-values (find-typedef name declarations)))
+(define (namespace-get-namespace namespace name)
+  (for/first ([(n v) (in-hash (namespace-namespaces namespace))]
+              #:when (equal? name n))
+    v))
